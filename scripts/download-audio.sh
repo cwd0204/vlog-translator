7cf524b3e3834eebdb498fdfadb9abb0c95beb01
cwd0204 <cwd0204@users.noreply.github.com>
2023-05-06 13:37:17 +0800

    Update download-audio.sh
    d

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -5,3 +5,5 @@ VIDEO_ID=$1
 
 yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s.%(ext)s" 2>&1
 
+# whisper speaking-french.wav --model large --language French --task translate
+# whisper China\ v\ Taiwan：\ explained\ \[gZ7hGoUEGCM\].m4a --model large --language 'zh'  --task translate --best_of 20
