eba5e26934469f0729464e1cfae6790fe6d6239b
cwd0204 <cwd0204@users.noreply.github.com>
2023-05-08 19:58:34 +0800

    Update download-audio.sh

--- scripts/download-audio.sh
+++ scripts/download-audio.sh
@@ -7,3 +7,4 @@ yt-dlp "https://www.youtube.com/watch?v=$VIDEO_ID" --format m4a -o "./tmp/%(id)s
 
 # whisper speaking-french.wav --model large --language French --task translate
 # whisper China\ v\ Taiwan：\ explained\ \[gZ7hGoUEGCM\].m4a --model large --language 'zh'  --task translate --best_of 20
+# ~/youtube ❯ whisper  基于LangChain实现ChatGPT交互式聊天\ \[n9Ye3GhsPKA\].m4a --language zh --model large-v2  --task translate  --best_of 20 --output_format srt                                                    4s 19:57:54
